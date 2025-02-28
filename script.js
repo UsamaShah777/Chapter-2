c177b883046aff39375f46a9001efca488176c0d
[Usamashah777] <[usamanedo@gmail.com]>
2025-02-28 16:18:59 +0500

    Chapter 2

---
+++ script.js
@@ -0,0 +1,84 @@
+
+
+
+// 1. Declare a variable called username.
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+// var userName= prompt("please enter Your Name");
+// alert("your name is "+ userName);
+// console.log(userName);
+// console.log(typeof userName);
+// console.log("-----------------------------------");
+
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var message=("Hello world");
+// console.log=(typeof  message);
+// console.log=(message);
+// alert(message);
+// console.log("------------------");
+
+
+
+// Student name
+
+
+// var Student= prompt("please enter Your Name");
+// alert("your name is "+ Student);
+// console.log(Student);
+// console.log(typeof Student);
+// console.log("-----------------------------------");
+
+
+// student age
+
+// var  num = prompt("please enter Your age");
+// alert("your age is "+ num);
+// console.log(num);
+// console.log(typeof num);
+// console.log("-----------------------------------");
+
+
+// student qaulifications
+
+// var study= prompt("please enter Your Qaulificatio");
+// alert("your Qaulificatio is "+ study);
+// console.log(study);
+// console.log(typeof study);
+// console.log("-----------------------------------");
+
+
+
+
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+
+// var pizza= prompt("Pizza")
+// alert("Pizza\nPizz\npiz\npi\np")
+// console.log(pizza)
+// console.log(typeof pizza)
+// console.log("---------------------------------------")
+
+
+// 7.Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box
+
+
+// var book = "A smarter way to learn JavaScript";
+// alert("The book is: " + book);
+
+
+
+        document.getElementById("content").innerText = "Yah! I can write HTML content through JavaScript";
+
+
+
+
